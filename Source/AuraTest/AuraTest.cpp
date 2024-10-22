0507fe5b33ea0b9a205f3c7f5462a069f3467633
Dimoonct <1134586513@qq.com>
2024-10-22 23:38:21 +0800

    Initial Project Files
    2024年10月22日23:37:46

---
+++ Source/AuraTest/AuraTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AuraTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AuraTest, "AuraTest" );
